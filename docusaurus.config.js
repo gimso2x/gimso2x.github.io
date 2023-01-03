3d5a099334651bcc3b5630c04b21b196f33e177b
Thebiz <thebiz@Thebizui-MacBookPro.local>
2023-01-03 09:53:03 +0900

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,80 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Gimso2x`s Devlog",
+  tagline: "Gimso2x`s Devlog",
+  url: "https://gimso2x.github.io",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "gimso2x", // Usually your GitHub org/user name.
+  projectName: "gimso2x.github.io", // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "ko",
+    locales: ["ko"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: false,
+        blog: {
+          routeBasePath: "/",
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Gimso2x`s Devlog",
+        logo: {
+          alt: "Gimso2x Logo",
+          src: "img/logo_blue.png",
+          srcDark: "img/logo.png",
+        },
+        items: [
+          { to: "/tags", label: "Tags", position: "left" },
+          { to: "/archive", label: "Archive", position: "left" },
+          {
+            href: "https://github.com/gimso2x",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        copyright: `Copyright © ${new Date().getFullYear()} Gimso2x, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
